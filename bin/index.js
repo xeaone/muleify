ce16460aff321dc069ac66831955249f1f73c3e4
Alexander Elias <alex.steven.elias@gmail.com>
2019-04-25 17:40:48 -0700

    prep for new version of cliy

--- bin/index.js
+++ bin/index.js
@@ -2,54 +2,11 @@
 
 const Operations = require('./operations');
 const Package = require('../package');
-const Muleify = require('../index');
-const Parse = require('./parse');
 const Cliy = require('cliy');
-const Path = require('path');
-const Fs = require('fs');
 
 (async function() {
 
     const program = new Cliy();
-
-    const Method = async function (argument, options) {
-    	const data = await Parse(argument);
-
-        program.log('\nMuleify\n', ['underline', 'cyan']);
-
-        if (options.pack) {
-            await Muleify.packer(data.input, data.output, options);
-    		program.log(`\tPacked: `, ['green']);
-        }
-
-        if (options.serve) {
-    		const server = await Muleify.server(data.input, data.output, options);
-    		program.log(`\tServed: ${server.hostname}:${server.port}`, ['green']);
-        }
-
-    	if (options.watch) {
-    		const watcher = await Muleify.watcher(data.input, data.output, options);
-
-    		watcher.on('error', function (error) {
-    			program.log(error.stack, ['red']);
-    		});
-
-    		watcher.on('change', function (path) {
-                if (options.pack) {
-    				Promise.resolve().then(function () {
-    	               return Muleify.packer(data.input, data.output, options);
-    				}).then(function () {
-    					program.log(`Changed: ${path}`, ['magenta']);
-    				}).catch(function (error) {
-    					program.log(error.stack, ['red']);
-    				});
-                }
-    		});
-    	}
-
-    	program.log(`Input: ${data.input}`, ['magenta']);
-    	program.log(`Output: ${data.output}`, ['magenta']);
-    };
 
     await program.setup({
     	name: Package.name,
--- bin/operations.js
+++ bin/operations.js
@@ -1,3 +1,47 @@
+'use strict';
+
+const Muleify = require('../index');
+const Parse = require('./parse');
+
+const Method = async function (argument, options) {
+    const program = this;
+	const data = await Parse(argument);
+
+    program.log('\nMuleify\n', ['underline', 'cyan']);
+
+    if (options.pack) {
+        // await Muleify.packer(data.input, data.output, options);
+		program.log(`\tPacked: `, ['green']);
+    }
+
+    if (options.serve) {
+		// const server = await Muleify.server(data.input, data.output, options);
+		program.log(`\tServed: ${server.hostname}:${server.port}`, ['green']);
+    }
+
+	if (options.watch) {
+		// const watcher = await Muleify.watcher(data.input, data.output, options);
+
+		// watcher.on('error', function (error) {
+		// 	program.log(error.stack, ['red']);
+		// });
+        //
+		// watcher.on('change', function (path) {
+        //     if (options.pack) {
+		// 		Promise.resolve().then(function () {
+	    //            return Muleify.packer(data.input, data.output, options);
+		// 		}).then(function () {
+		// 			program.log(`Changed: ${path}`, ['magenta']);
+		// 		}).catch(function (error) {
+		// 			program.log(error.stack, ['red']);
+		// 		});
+        //     }
+		// });
+	}
+
+	program.log(`Input: ${data.input}`, ['magenta']);
+	program.log(`Output: ${data.output}`, ['magenta']);
+};
 
 module.exports = {
 	// path: {
@@ -12,8 +56,6 @@ module.exports = {
 		description: 'Packs ',
 		method: Method,
 		operations: [
-			module.exports.Serve,
-			module.exports.Watch,
         	{
         		key: 'b',
         		name: 'bundle',
@@ -31,7 +73,9 @@ module.exports = {
         		name: 'transpile',
         		description: 'Transpile the output',
         		method: async function () { return true; }
-        	}
+        	},
+			module.exports.Serve,
+			module.exports.Watch
 		]
 	},
 	Serve: {
@@ -41,8 +85,6 @@ module.exports = {
 		method: Method,
 		operations: [
 			// module.exports.path,
-			module.exports.Watch,
-			module.exports.Pack,
         	{
         		key: 's',
         		name: 'spa',
@@ -54,7 +96,9 @@ module.exports = {
         		name: 'cors',
         		description: 'Enables cross origin resource sharing mode',
         		method: async function () { return true; }
-        	}
+        	},
+			module.exports.Pack,
+			module.exports.Watch
 		]
 	},
 	Watch: {
@@ -65,7 +109,6 @@ module.exports = {
 		operations: [
 			module.exports.Pack,
 			module.exports.Serve,
-			// module.exports.path,
 		]
 	},
     Map: {
@@ -75,12 +118,12 @@ module.exports = {
 		method: async function (argument, options) {
 			const data = await Parse(argument);
 
-			program.log('\nMuleify Mapping\n', ['underline', 'cyan']);
+			this.log('\nMuleify Mapping\n', ['underline', 'cyan']);
 
 			await Muleify.map(data.input, data.output, options);
 
-			program.log(`Input: ${data.input}`, ['magenta']);
-			program.log(`Output: ${data.output}`, ['magenta']);
+			this.log(`Input: ${data.input}`, ['magenta']);
+			this.log(`Output: ${data.output}`, ['magenta']);
 		},
 		operations: [
         	{
@@ -98,12 +141,12 @@ module.exports = {
 		method: async function (argument, options) {
 			const data = await Parse(argument);
 
-			program.log('\nMuleify Encamping\n', ['underline', 'cyan']);
+			this.log('\nMuleify Encamping\n', ['underline', 'cyan']);
 
 			await Muleify.encamp(data.input, data.output);
 
-			program.log(`Input: ${data.input}`, ['magenta']);
-			program.log(`Output: ${data.output}`, ['magenta']);
+			this.log(`Input: ${data.input}`, ['magenta']);
+			this.log(`Output: ${data.output}`, ['magenta']);
 		}
 	},
 	InstallSass: {
@@ -111,9 +154,9 @@ module.exports = {
 		name: 'install-sass',
 		description: 'Installs sass/scss compiler (might require sudo)',
 		method: async function (argument, options) {
-			program.log('Installing...', ['white']);
+			this.log('Installing...', ['white']);
 			const result = await Muleify.sass();
-			program.log(result, ['white']);
+			this.log(result, ['white']);
 		}
 	}
 };
