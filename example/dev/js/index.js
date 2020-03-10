3eff9cdc4d17c83b9bb8e66140f502303658204f
Alexander Elias <alex.steven.elias@gmail.com>
2020-03-09 18:02:42 -0700

    updated packages

--- example/dev/js/index.js
+++ example/dev/js/index.js
@@ -1,75 +1,79 @@
-var _typeof2 = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+function _call(body, then, direct) {
+  if (direct) {
+    return then ? then(body()) : body();
+  }
 
-var _typeof = typeof Symbol === "function" && _typeof2(Symbol.iterator) === "symbol" ? function (obj) {
-	return typeof obj === "undefined" ? "undefined" : _typeof2(obj);
-} : function (obj) {
-	return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj === "undefined" ? "undefined" : _typeof2(obj);
-};
+  try {
+    var result = Promise.resolve(body());
+    return then ? result.then(then) : result;
+  } catch (e) {
+    return Promise.reject(e);
+  }
+}
 
 function _await(value, then, direct) {
-	if (direct) {
-		return then ? then(value) : value;
-	}value = Promise.resolve(value);return then ? value.then(then) : value;
-}function _call(body, then, direct) {
-	if (direct) {
-		return then ? then(body()) : body();
-	}try {
-		var result = Promise.resolve(body());return then ? result.then(then) : result;
-	} catch (e) {
-		return Promise.reject(e);
-	}
-}(function (global, factory) {
-	(typeof exports === 'undefined' ? 'undefined' : _typeof(exports)) === 'object' && typeof module !== 'undefined' ? module.exports = factory() : typeof define === 'function' && define.amd ? define('Index', factory) : global.Index = factory();
+  if (direct) {
+    return then ? then(value) : value;
+  }
+
+  if (!value || !value.then) {
+    value = Promise.resolve(value);
+  }
+
+  return then ? value.then(then) : value;
+}
+
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+(function (global, factory) {
+  (typeof exports === "undefined" ? "undefined" : _typeof(exports)) === 'object' && typeof module !== 'undefined' ? module.exports = factory() : typeof define === 'function' && define.amd ? define('Index', factory) : (global = global || self, global.Index = factory());
 })(this, function () {
-	'use strict';
+  'use strict';
 
-	var two = 2;
+  var two = 2;
+  var one = 1;
 
-	var one = 1;
+  var promise = function promise() {
+    return Promise.resolve('adding');
+  };
 
-	var promise = function promise() {
-		return Promise.resolve('adding');
-	};
+  var add = function add(numOne, numTwo) {
+    return _call(promise, function (message) {
+      console.log(message);
+      return numOne + numTwo;
+    });
+  };
+  /*
+  	@banner
+  	I am still here.
+  */
 
-	var add = function add(numOne, numTwo) {
-		return _call(promise, function (message) {
-			console.log(message);
-			return numOne + numTwo;
-		});
-	};
+  /*
+  	@preserve
+  	I am still here again
+  */
 
-	/*
- 	@banner
- 	I am still here.
- */
 
-	/*
- 	@preserve
- 	I am still here again
- */
+  Promise.resolve().then(function () {
+    return _await(add(one, two), function (sum) {
+      console.log("".concat(sum));
+    });
+  }).catch(console.error);
+  console.log('bundle');
+  var index_b_t = {
+    add: add
+  };
+  /*
+  const p = Promise.resolve().then(function () {
+      return 'hello world';
+  });
+  	(async function () {
+  	    const r = await p();
+      console.log(r);
+  	}()).catch(function (e) {
+      console.log(e);
+  });
+  */
 
-	(function () {
-		return _await(add(one, two), function (sum) {
-			console.log('' + sum);
-		});
-	})().catch(function (error) {
-		console.error(error);
-	});
-
-	console.log('bundle');
-
-	var index_b_t = { add: add };
-	/*
- const p = Promise.resolve().then(function () {
-     return 'hello world';
- });
- 	(async function () {
- 	    const r = await p();
-     console.log(r);
- 	}()).catch(function (e) {
-     console.log(e);
- });
- */
-
-	return index_b_t;
+  return index_b_t;
 });
--- example/dev/js/zero.js
+++ example/dev/js/zero.js
@@ -1,9 +1,7 @@
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+(function (factory) {
+  typeof define === 'function' && define.amd ? define('Zero', factory) : factory();
+})(function () {
+  'use strict';
 
-(function (global, factory) {
-	(typeof exports === 'undefined' ? 'undefined' : _typeof(exports)) === 'object' && typeof module !== 'undefined' ? factory() : typeof define === 'function' && define.amd ? define('Zero', factory) : factory();
-})(this, function () {
-	'use strict';
-
-	console.log('zero');
+  console.log('zero');
 });
