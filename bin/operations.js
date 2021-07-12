c8573d8b3dc2dd5aa284b30b90e5eb89f256f8ec
Alexander Elias <alex.steven.elias@gmail.com>
2021-07-12 10:49:20 -0700

    updated packages

--- bin/operations.js
+++ bin/operations.js
@@ -4,133 +4,133 @@ const Muleify = require('../index');
 const Path = require('./path.js');
 
 module.exports.Pack = {
-	key: 'p',
-	name: 'pack',
-	description: 'Packs folder or file',
-	options: [ 'input', 'output' ],
-	async handler (options, results) {
-		const input = await Path.call(this, options.input);
-		const output = await Path.call(this, options.output);
-		await Muleify.packer(input, output, results);
-		this.log(`Packed: ${input} to ${output}`, [ 'green' ]);
-		return true;
-	},
-	operations: [
-		{
-			key: 'b',
-			name: 'bundle',
-			description: 'Bundles the output',
-			async handler () { return true; }
-		},
-		{
-			key: 'm',
-			name: 'minify',
-			description: 'Minifies the output',
-			async handler () { return true; }
-		},
-		{
-			key: 't',
-			name: 'transpile',
-			description: 'Transpile the output',
-			async handler () { return true; }
-		},
-		{
-			key: 'w',
-			name: 'watch',
-			description: 'Watches folder or file',
-			async handler (options, results) {
-				const self = this;
-				const input = await Path.call(this, options.input);
-				const output = await Path.call(this, options.output);
-				const watcher = await Muleify.watcher(input, results);
+    key: 'p',
+    name: 'pack',
+    description: 'Packs folder or file',
+    options: [ 'input', 'output' ],
+    async handler (options, results) {
+        const input = await Path.call(this, options.input);
+        const output = await Path.call(this, options.output);
+        await Muleify.packer(input, output, results);
+        this.log(`Packed: ${input} to ${output}`, [ 'green' ]);
+        return true;
+    },
+    operations: [
+        {
+            key: 'b',
+            name: 'bundle',
+            description: 'Bundles the output',
+            async handler () { return true; }
+        },
+        {
+            key: 'm',
+            name: 'minify',
+            description: 'Minifies the output',
+            async handler () { return true; }
+        },
+        {
+            key: 't',
+            name: 'transpile',
+            description: 'Transpile the output',
+            async handler () { return true; }
+        },
+        {
+            key: 'w',
+            name: 'watch',
+            description: 'Watches folder or file',
+            async handler (options, results) {
+                const self = this;
+                const input = await Path.call(this, options.input);
+                const output = await Path.call(this, options.output);
+                const watcher = await Muleify.watcher(input, results);
 
-				watcher.on('error', function (error) {
-					self.error(error.stack);
-				});
+                watcher.on('error', function (error) {
+                    self.error(error.stack);
+                });
 
-				watcher.on('change', function (path) {
-					Promise.resolve().then(function () {
-						return Muleify.packer(input, output, results);
-					}).then(function () {
-						self.log(`Changed: ${path}`, [ 'magenta' ]);
-					}).catch(function (error) {
-						self.error(error.stack);
-					});
-				});
+                watcher.on('change', function (path) {
+                    Promise.resolve().then(function () {
+                        return Muleify.packer(input, output, results);
+                    }).then(function () {
+                        self.log(`Changed: ${path}`, [ 'magenta' ]);
+                    }).catch(function (error) {
+                        self.error(error.stack);
+                    });
+                });
 
-				self.log(`Watched: ${input} to ${output}`, [ 'green' ]);
+                self.log(`Watched: ${input} to ${output}`, [ 'green' ]);
 
-				return true;
-			}
-		}
-	]
+                return true;
+            }
+        }
+    ]
 };
 
 module.exports.Serve = {
-	key: 's',
-	name: 'serve',
-	description: 'Serves folder or file',
-	options: [ 'path' ],
-	async handler (options, results) {
-		const path = await Path.call(this, options.path || options.output);
-		const server = await Muleify.server(path, results);
-		this.log(`Served: ${server.hostname}:${server.port}`, [ 'green' ]);
-		return true;
-	},
-	operations: [
-		{
-			key: 's',
-			name: 'spa',
-			description: 'Enables single page application mode',
-			async handler () { return true; }
-		},
-		{
-			key: 'c',
-			name: 'cors',
-			description: 'Enables cross origin resource sharing mode',
-			async handler () { return true; }
-		}
-	]
+    key: 's',
+    name: 'serve',
+    description: 'Serves folder or file',
+    options: [ 'path' ],
+    async handler (options, results) {
+        const path = await Path.call(this, options.path || options.output);
+        const server = await Muleify.server(path, results);
+        this.log(`Served: ${server.host}:${server.port}`, [ 'green' ]);
+        return true;
+    },
+    operations: [
+        {
+            key: 's',
+            name: 'spa',
+            description: 'Enables single page application mode',
+            async handler () { return true; }
+        },
+        {
+            key: 'c',
+            name: 'cors',
+            description: 'Enables cross origin resource sharing mode',
+            async handler () { return true; }
+        }
+    ]
 };
 
 module.exports.Map = {
-	key: 'm',
-	name: 'map',
-	options: [ 'input', 'output', 'domain' ],
-	description: 'Creates XML sitemap',
-	async handler (options) {
-		this.log('Mapping', [ 'underline', 'cyan' ]);
-		const input = await Path.call(this, options.input);
-		const output = await Path.call(this, options.output);
-		await Muleify.map(input, output, options.domain);
-		this.log(`Input: ${input}`, [ 'magenta' ]);
-		this.log(`Output: ${output}`, [ 'magenta' ]);
-	},
-	operations: []
+    key: 'm',
+    name: 'map',
+    options: [ 'input', 'output', 'domain' ],
+    description: 'Creates XML sitemap',
+    async handler (options) {
+        this.log('Mapping', [ 'underline', 'cyan' ]);
+        const input = await Path.call(this, options.input);
+        const output = await Path.call(this, options.output);
+        await Muleify.map(input, output, options.domain);
+        this.log(`Input: ${input}`, [ 'magenta' ]);
+        this.log(`Output: ${output}`, [ 'magenta' ]);
+    },
+    operations: []
 };
 
 module.exports.Encamp = {
-	key: 'e',
-	name: 'encamp',
-	options: [ 'input', 'output' ],
-	description: 'Creates folders and files from a json file',
-	async handler (options) {
-		this.log('Muleify Encamping', [ 'underline', 'cyan' ]);
-		const input = await Path.call(this, options.input);
-		const output = await Path.call(this, options.output);
-		await Muleify.encamp(input, output);
-		this.log(`Input: ${options.input}`, [ 'magenta' ]);
-		this.log(`Output: ${options.output}`, [ 'magenta' ]);
-	}
+    key: 'e',
+    name: 'encamp',
+    options: [ 'input', 'output' ],
+    description: 'Creates folders and files from a json file',
+    async handler (options) {
+        this.log('Muleify Encamping', [ 'underline', 'cyan' ]);
+        const input = await Path.call(this, options.input);
+        const output = await Path.call(this, options.output);
+        await Muleify.encamp(input, output);
+        this.log(`Input: ${options.input}`, [ 'magenta' ]);
+        this.log(`Output: ${options.output}`, [ 'magenta' ]);
+    }
 };
 
 module.exports.InstallSass = {
-	key: 'i',
-	name: 'install-sass',
-	description: 'Installs sass/scss compiler (might require sudo)',
-	async handler () {
-		this.log('Installing...', [ 'white' ]);
-		const data = await Muleify.sass();
-		this.log(data, [ 'white' ]);
-	}
+    key: 'i',
+    name: 'install-sass',
+    description: 'Installs sass/scss compiler (might require sudo)',
+    async handler () {
+        this.log('Installing...', [ 'white' ]);
+        const data = await Muleify.sass();
+        this.log(data, [ 'white' ]);
+    }
 };
--- index.js
+++ index.js
@@ -2,125 +2,117 @@ const Transform = require('./lib/transform');
 const Sitemap = require('./lib/sitemap');
 const Global = require('./lib/global');
 const Observey = require('observey');
-const Servey = require('servey');
 const Porty = require('porty');
 const Path = require('path');
 const Fsep = require('fsep');
+const Nety = require('nety');
 
 const LB = Global.lb;
 const IGNOREABLES = Global.ignoreables;
 
+const { Server, File, Normalize, Preflight } = Nety.HttpServer;
+
 const directory = async function (input, output, options) {
-	let before = [];
-	let after = [];
+    let before = [];
+    let after = [];
 
-	const paths = await Fsep.walk({
-		path: input,
-		ignoreDot: true,
-		filters: IGNOREABLES
-	});
+    const paths = await Fsep.walk({
+        path: input,
+        ignoreDot: true,
+        filters: IGNOREABLES
+    });
 
-	paths.forEach(path => {
-		if (LB.test(path)) {
-			before.push(path);
-		} else {
-			after.push(path);
-		}
-	});
+    paths.forEach(path => {
+        if (LB.test(path)) {
+            before.push(path);
+        } else {
+            after.push(path);
+        }
+    });
 
-	await Promise.all(before.map(async path => {
-		return Transform(Path.join(input, path), Path.join(output, path), options);
-	}));
+    await Promise.all(before.map(async path => {
+        return Transform(Path.join(input, path), Path.join(output, path), options);
+    }));
 
-	await Promise.all(after.map(async path => {
-		return Transform(Path.join(input, path), Path.join(output, path), options);
-	}));
+    await Promise.all(after.map(async path => {
+        return Transform(Path.join(input, path), Path.join(output, path), options);
+    }));
 
 };
 
 const file = async function (input, output, options) {
-	await Transform(input, output, options);
+    await Transform(input, output, options);
 };
 
 exports.packer = async function (input, output, options) {
 
-	input = Path.resolve(process.cwd(), input);
+    input = Path.resolve(process.cwd(), input);
 
-	if (!Fsep.existsSync(input)) {
-		throw new Error(`Input path does not exist ${input}`);
-	}
+    if (!Fsep.existsSync(input)) {
+        throw new Error(`Input path does not exist ${input}`);
+    }
 
-	output = Path.resolve(process.cwd(), output);
+    output = Path.resolve(process.cwd(), output);
 
-	Global.input = input; // TODO find a way to remove this
+    Global.input = input; // TODO find a way to remove this
 
-	const stat = await Fsep.stat(input);
+    const stat = await Fsep.stat(input);
 
-	if (stat.isFile()) {
-		await file(input, output, options);
-	} else if (stat.isDirectory()) {
-		await directory(input, output, options);
-	} else {
-		throw new Error(`Input is not a file or direcotry ${input}`);
-	}
+    if (stat.isFile()) {
+        await file(input, output, options);
+    } else if (stat.isDirectory()) {
+        await directory(input, output, options);
+    } else {
+        throw new Error(`Input is not a file or direcotry ${input}`);
+    }
 };
 
 exports.watcher = async function (path, options) {
-	const observer = new Observey({ path: path });
-	await observer.open();
-	return observer;
+    const observer = new Observey({ path: path });
+    await observer.open();
+    return observer;
 };
 
-exports.server = async function (path, options) {
-	const port = await Porty.find(8080);
+exports.server = async function (folder, options) {
+    const { spa } = options || {};
+    const port = await Porty.find(8080);
 
-	const server = new Servey({
-		port: port,
-		cache: false,
-		cors: options.cors,
-		hostname: 'localhost',
-		routes: [
-			{
-				path: '*',
-				method: 'get',
-				handler: async function (context) {
-					return await context.tool.static({
-						folder: path,
-						spa: options.spa,
-						path: context.url.pathname
-					});
-				}
-			}
-		]
-	});
+    const file = new File();
+    const normalize = new Normalize();
+    const preflight = new Preflight();
+    const server = new Server({ port, debug: true, host: 'localhost' });
 
-	await server.open();
+    await server.add(normalize);
+    await server.add(preflight);
+    await server.add(file);
+    await server.get(async context => context.file({ spa, folder, path: context.url.pathname }));
+    await server.open();
 
-	return server;
+    return server;
 };
 
 exports.encamp = async function (input, output) {
-	const data = await Fsep.readFile(input);
-	await Fsep.scaffold(output, JSON.parse(data));
+    const data = await Fsep.readFile(input);
+    await Fsep.scaffold(output, JSON.parse(data));
 };
 
 exports.map = async function (input, output, domain) {
-	const paths = await Fsep.walk({
-		path: input,
-		ignoreDot: true,
-		filters: IGNOREABLES
-	});
+    const paths = await Fsep.walk({
+        path: input,
+        ignoreDot: true,
+        filters: IGNOREABLES
+    });
 
-	const sitemap = await Sitemap(paths, domain);
-	const path = Path.join(output, 'sitemap.xml');
+    const sitemap = await Sitemap(paths, domain);
+    const path = Path.join(output, 'sitemap.xml');
 
-	await Fsep.outputFile(path, sitemap);
+    await Fsep.outputFile(path, sitemap);
 };
 
 exports.sass = async function () {
-	return await Terminal({
-		cmd: 'npm',
-		args: [ 'i', '--no-save', 'node-sass' ],
-		cwd: __dirname
-	});
+    return await Terminal({
+        cmd: 'npm',
+        args: [ 'i', '--no-save', 'node-sass' ],
+        cwd: __dirname
+    });
 };
